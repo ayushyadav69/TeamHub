b50ea1e5c4bf16a15412461fba3199d5c5bc5006
Ayush Yadav <ayush.dms.1415@gmail.com>
2026-03-24 17:58:54 +0530

    added if DB does not have employees try API

--- TeamHub/Data/Repository/DefaultEmployeeRepository.swift
+++ TeamHub/Data/Repository/DefaultEmployeeRepository.swift
@@ -42,8 +42,35 @@ final class DefaultEmployeeRepository: EmployeeRepository {
             return response.data.map { $0.toEmployee() }
         }
         
-        // CASE 2: Default OR Offline
-        return try await local.fetch(query: query, page: page)
+        // CASE 2: Normal list (DB-first)
+        
+        let localData = try await local.fetch(query: nil, page: page)
+        
+        // If DB has data → return it
+        if !localData.isEmpty {
+            return localData
+        }
+        
+        // If DB empty AND online → fetch from API
+        if networkMonitor.isConnected {
+            
+            let response = try await remote.fetchEmployees(
+                query: nil,
+                page: page
+            )
+            
+            let employees = response.data.map { $0.toEmployeeDetail(dateParser: dateParser) }
+            
+            // Save to DB
+            for employee in employees {
+                try await local.insert(employee)
+            }
+            
+            // Return from DB (single source of truth)
+            return try await local.fetch(query: nil, page: page)
+        }
+        
+        return []
     }
     
     func addEmployee(_ employee: EmployeeDetail) async throws {
